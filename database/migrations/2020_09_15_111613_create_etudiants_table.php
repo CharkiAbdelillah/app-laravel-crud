556fde70b7666a2911df516f15fa284881e7bd78
root <root@nginx.iibs3utpxfhezh2d50y0ohp1rg.jx.internal.cloudapp.net>
2020-09-15 11:18:37 +0000

    add config database(.env)

---
+++ database/migrations/2020_09_15_111613_create_etudiants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+             $table->increments('id'); 
+             $table->string('nom'); 
+            $table->string('email'); 
+            $table->text('branche');  
+            $table->text('image'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
