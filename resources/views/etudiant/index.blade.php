ee82132f3c5f6c3e5d5fedd15edddb9300d2c7c3
root <root@nginx.iibs3utpxfhezh2d50y0ohp1rg.jx.internal.cloudapp.net>
2020-09-19 17:25:57 +0000

    add config in index.blade.php

--- resources/views/etudiant/index.blade.php
+++ resources/views/etudiant/index.blade.php
@@ -4,17 +4,21 @@
 @section('content')
 <!-- hadi tat gol sir ldik fichier layout ofdik blasa fin drt yeild('content') ktb hadxi -->
  
-<br> 
-  <div class="col-md-11.5 text-right">
-   <a class="btn btn-danger" href="{{URL::to('deletehaha')}}" onclick="return confirm('confirmer !!')">Supprimer La liste</a>
- </div> 
+<br>
     <div class="row">
-    <div class="col-md-6">
-        <h2>Listes des etudiants de EST Sidi Bennour<h2>  
+    <div class="col-lg-12 margin-tb">
+     <div class="pull-left">  
+      <h2>Listes des etudiants de EST Sidi Bennour<h2>  
        </div>
-<br><br><br>
-    <div class="col-md-3 text-center" >
-    <form action="/search" methode="get">
+<div class="pull-right">
+<img src="https://www.tawjihpress.com/wp-content/uploads/2019/04/EST-Sidi-Bennour.jpg"  height="220px;" width="290px;">
+</div>
+</div>
+</div>
+<br><br><br><br>
+<div class="row justify-content-md-center">
+    <div class="col-lg-7" style="text-align:center;">
+    <form action="/search" text-align="center" methode="get">
       <div class="input-group">
       <input type="search" name="search"  class="form-control">
        <span class="input-group-prepend">
@@ -23,9 +27,12 @@
    </div>
    </form>
     </div>
-     <div class="col-md-2 text-right">
+     <div class="col-lg-3">
        <a class="btn btn-success" href="{{ route('create.etudiant')}}">Ajouter un nouveau etudiant</a>
      </div>
+   <div class="col">
+     <a class="btn btn-danger" href="{{URL::to('deletehaha')}}" onclick="return confirm('confirmer !!')">Supprimer La liste</a>
+    </div> 
    </div> 
      <!-- dyl class="row" bax dikxi li class row maybanx bl khdr fax creer xi haja .bsbab dik success li drna lta7t -->
     @if($message=Session::get('success'))
@@ -33,15 +40,15 @@
       <p>{{$message}}</p>
       </div>
      @endif
-   
+
 <table class="table table-bordered table-striped">
  <tr>
   <th width="8%">Photo</th>
   <th width="16%">Nom Complet</th>
- <th width="12%">Genre</th>
+  <th width="12%">Genre</th>
   <th width="16%">Email</th>
-   <th width="10%">Branche</th>
- <th width="28%">Action</th>
+  <th width="10%">Branche</th>
+  <th width="28%">Action</th>
  </tr>
   
     
@@ -54,8 +61,8 @@
     <td> {{ $pro->branche }} </td>
     <td>
        <a class="btn btn-info" href="{{URL::to('show/etudiant/'.$pro->id)}}">Montrer</a>
-         <a class="btn btn-primary" href="{{URL::to('edit/etudiant/'.$pro->id)}}">Modifier</a>
-           <a class="btn btn-danger" href="{{URL::to('delete/etudiant/'.$pro->id)}}" onclick="return confirm('confirmer !!')">Supprimer</a>
+       <a class="btn btn-primary" href="{{URL::to('edit/etudiant/'.$pro->id)}}">Modifier</a>
+       <a class="btn btn-danger" href="{{URL::to('delete/etudiant/'.$pro->id)}}" onclick="return confirm('confirmer !!')">Supprimer</a>
      </td>
      </tr>
    @endforeach
