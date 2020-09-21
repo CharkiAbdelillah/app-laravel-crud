97caa220fd9073c5ed77adc2a1d01dab0a68db2a
root <root@nginx.iibs3utpxfhezh2d50y0ohp1rg.jx.internal.cloudapp.net>
2020-09-21 09:03:09 +0000

    add design in main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,20 +1,29 @@
+
 <html>
  <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel 5.8 Crud Tutorial</title>
+  <title>Laravel Crud</title>
   <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+  <style>
+body {
+border: 2px solid black;
+  padding: 25px;
+  background: lightblue url("https://wallpaperaccess.com/full/1863448.jpg") no-repeat fixed center; 
+}
+.btn{font-size: 24px;
+background-color: #e7e7e7; color: black;
+}
+</style>
+</head>
  </head>
  <body>
-  <div class="container">    
-     <br />
-      <div class="row">
-     <h1>Ajouter une etudiant</h1>
-      <a class="btn btn-info" href="{{ route('etudiant.index') }}">Ajouter une etudiant</a>
-     </div>
-     //@yield('main')
-    </div>
+  <div class="container text-center">  
+<div class="col-lg">
+      <a class="btn btn-info"  href="{{ route('etudiant.index') }}">Voir la liste des etudiants</a>
+</div>  
+</div>
  </body>
 </html>
